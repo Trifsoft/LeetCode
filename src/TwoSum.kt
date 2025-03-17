68e50780946508f98cee07d0da9c17cf68a1d65d
Nikola Trifunovic <nikola.trifunovic14@gmail.com>
2025-03-17 19:19:50 +0100

    Initial commit

---
+++ src/TwoSum.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val pairList =
+            nums.mapIndexed{ index, i -> Pair(i, index) }
+                .sortedBy { it.first }
+        var i = 0
+        var j = pairList.size - 1
+        while(i < j) {
+            if(pairList[i].first + pairList[j].first > target) {
+                j--
+            }
+            else if(pairList[i].first + pairList[j].first < target) {
+                i++
+            }
+            else {
+                return intArrayOf(pairList[i].second, pairList[j].second)
+            }
+        }
+        return intArrayOf()
+    }
+}
